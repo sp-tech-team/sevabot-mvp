f5f6e5255a76fe53c4a2f47352d4133a72b7966b
swapnil18800 <swapnil18800@gmail.com>
2025-08-31 12:18:01 +0530

    changed listening port for nginx

--- main.py
+++ main.py
@@ -107,7 +107,7 @@ if __name__ == "__main__":
     uvicorn.run(
         "main:app",
         host="0.0.0.0",
-        port=8000,
+        port=8001,
         workers=1,
         reload=False,
         access_log=True,
