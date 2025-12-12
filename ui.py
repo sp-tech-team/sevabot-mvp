66b1781ca70b3c9aa239ad41b7299562723d4c1a
swapnil18800 <swapnil18800@gmail.com>
2025-12-12 12:28:07 +0530

    gradio fix 18 for css

--- ui.py
+++ ui.py
@@ -49,6 +49,7 @@ def create_gradio_interface():
     with gr.Blocks(
         theme=gr.themes.Soft(), 
         title="Isha Sevabot",
+        head=get_favicon_link(),
         css=get_main_app_css()
     ) as demo:
         
